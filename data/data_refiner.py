1d073b89937fd6396242e63f1b661ecf0571cf2c
Simon Huber <33151777+simonhuber95@users.noreply.github.com>
2021-09-07 09:58:30 +0200

    add combined team stats

---
+++ data/data_refiner.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+
+columns = ['Rk', 'Team', 'Games', 'Points', 'Yds', 'Playes', 'Y/P', 'Turnovers', 'Fumbles', '1stD', 'PassCmp',
+       'PassAtt', 'PassYds', 'PassTD', 'PassInt', 'PassNY/A', 'Pass1stD', 'RushAtt', 'RushYds', 'RushTD',
+       'RushY/A', 'Rush1stD', 'Penalties', 'PenYds', '1stPy', 'Sc%', 'Turnover%', 'EXP']
+
+off_columns =  list(map(lambda x: 'Off' + x if x not in ['Team', 'Games', 'Rk', 'EXP'] else x, columns))
+def_columns =  list(map(lambda x: 'Def' + x if x not in ['Team', 'Games', 'Rk', 'EXP'] else x, columns))
+
+def combine_stats(year):
+    def_path = "data\defense\DefensePerGame{}.csv".format(year)
+    off_path = "data\offense\OffensePerGame{}.csv".format(year)
+    def_df = pd.read_csv(def_path, skiprows=2, skipfooter=3, names = def_columns, engine = 'python', index_col = 'Team')
+    def_df = def_df.drop(['Games', 'Rk', 'EXP'], axis = 1)
+
+    off_df = pd.read_csv(off_path, skiprows=2, skipfooter=3, names = off_columns, engine = 'python', index_col = 'Team')
+    off_df = off_df.drop(['Games','Rk', 'EXP'], axis = 1)
+
+    team_df = pd.concat([off_df, def_df], axis=1)
+    team_df['Year'] = year
+    print(team_df)
+    return team_df
+
+for year in range(2001,2021):
+    path = "data/team/TeamPerGame{}.csv".format(year)
+    team_df = combine_stats(year)
+    team_df.to_csv(path)
