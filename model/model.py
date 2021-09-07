a9d422404f35caffcc60fa2c9ae2e68a4a8f63b3
Simon Huber <33151777+simonhuber95@users.noreply.github.com>
2021-09-07 14:56:52 +0200

    add k-fold

--- model/model.py
+++ model/model.py
@@ -2,6 +2,10 @@ import keras
 import pandas as pd
 from keras.layers import Dense, Dropout
 from keras import Sequential
+from keras.losses import sparse_categorical_crossentropy
+from keras.optimizers import Adam
+from sklearn.model_selection import KFold
+import numpy as np
 
 data_path = "data/team/TeamPerGame{}.csv".format(2020)
 df = pd.read_csv(data_path)
@@ -9,12 +13,66 @@ df = df.drop(['Team','Year'], axis = 1)
 input_shape = len(df.columns)*2 + 2 
 print(df.columns, input_shape)
 
+def load_data():
+    
 
-define_model(input_shape, layers = [], dropout = True):
-
+def define_model(input_shape, layers = [], dropout = True):
     model = Sequential
     model.add(BatchNormalization(input_shape=input_shape))
     for neurons in layers:
         model.add(Dense(neurons, activation="relu"))
         if dropout:
             model.add(Dropout(0.5))
+    model.add(Dense(2, activation='softmax'))
+	# Compile model
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+
+# Define per-fold score containers
+acc_per_fold = []
+loss_per_fold = []
+
+# Merge inputs and targets
+inputs = np.concatenate((input_train, input_test), axis=0)
+targets = np.concatenate((target_train, target_test), axis=0)
+
+# Define the K-fold Cross Validator
+kfold = KFold(n_splits=num_folds, shuffle=True)
+
+# K-fold Cross Validation model evaluation
+fold_no = 1
+for train, test in kfold.split(inputs, targets):
+
+  define_model(input_shape=(98,), layers=[128,256,256,128,64,32,16])
+
+
+  # Generate a print
+  print('------------------------------------------------------------------------')
+  print(f'Training for fold {fold_no} ...')
+
+  # Fit data to model
+  history = model.fit(inputs[train], targets[train],
+              batch_size=batch_size,
+              epochs=no_epochs,
+              verbose=verbosity)
+
+  # Generate generalization metrics
+  scores = model.evaluate(inputs[test], targets[test], verbose=0)
+  print(f'Score for fold {fold_no}: {model.metrics_names[0]} of {scores[0]}; {model.metrics_names[1]} of {scores[1]*100}%')
+  acc_per_fold.append(scores[1] * 100)
+  loss_per_fold.append(scores[0])
+
+  # Increase fold number
+  fold_no = fold_no + 1
+
+# == Provide average scores ==
+print('------------------------------------------------------------------------')
+print('Score per fold')
+for i in range(0, len(acc_per_fold)):
+  print('------------------------------------------------------------------------')
+  print(f'> Fold {i+1} - Loss: {loss_per_fold[i]} - Accuracy: {acc_per_fold[i]}%')
+print('------------------------------------------------------------------------')
+print('Average scores for all folds:')
+print(f'> Accuracy: {np.mean(acc_per_fold)} (+- {np.std(acc_per_fold)})')
+print(f'> Loss: {np.mean(loss_per_fold)}')
+print('------------------------------------------------------------------------')
